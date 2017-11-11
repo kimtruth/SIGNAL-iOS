e6408f0fa0e084d3d49634ad6ee772e501e4d112
j28150 <j28150@naver.com>
2017-11-11 17:40:43 +0900

    ViewController를 LoginViewController로 변경

--- Signal/AppDelegate.swift
+++ Signal/AppDelegate.swift
@@ -19,7 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         window.backgroundColor = .white
         window.makeKeyAndVisible()
         
-        window.rootViewController = ViewController()
+        window.rootViewController = LoginViewController()
         
         self.window = window
         return true
--- Signal/LoginViewController.swift
+++ Signal/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  Signal
 //
 //  Created by Truth on 2017. 11. 11..
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
