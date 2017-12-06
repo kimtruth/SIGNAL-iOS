08aa204a16e9efdc41f2882d26e6aa442e1a8130
j28150 <j28150@naver.com>
2017-12-06 19:26:51 +0900

    전역 변수 사용을 위한 Global 추가

---
+++ Signal/Sources/Global.swift
@@ -0,0 +1,12 @@
+//
+//  Global.swift
+//  Signal
+//
+//  Created by Truth on 2017. 12. 6..
+//  Copyright © 2017년 k1mtruth. All rights reserved.
+//
+
+import Foundation
+
+/// Global Variables
+var user: User?
