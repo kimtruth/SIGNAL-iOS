84e690e0b5c7fbf78897472ed3bac704eda70bef
j28150 <j28150@naver.com>
2017-12-06 19:08:35 +0900

    UserType 타입 추가

---
+++ Signal/Sources/Types/UserType.swift
@@ -0,0 +1,12 @@
+//
+//  UserType.swift
+//  Signal
+//
+//  Created by Truth on 2017. 12. 6..
+//  Copyright © 2017년 k1mtruth. All rights reserved.
+//
+
+enum UserType: String {
+  case signaler
+  case publisher
+}
