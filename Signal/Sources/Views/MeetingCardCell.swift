2100c93af00eceaee8b4440e3070f4459a2b79ac
j28150 <j28150@naver.com>
2017-12-07 11:37:58 +0900

    MeetingCardCell 클래스 추가

---
+++ Signal/Sources/Views/MeetingCardCell.swift
@@ -0,0 +1,20 @@
+//
+//  MeetingCardCell.swift
+//  Signal
+//
+//  Created by Truth on 2017. 12. 7..
+//  Copyright © 2017년 k1mtruth. All rights reserved.
+//
+
+import UIKit
+
+class MeetingCardCell: UICollectionViewCell {
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+  
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+}
